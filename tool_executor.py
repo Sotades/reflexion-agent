0489b6a8deca0e18e8a539ea7d3aa56dab7cdf80
Sotades <anthonyjbateman@gmail.com>
2024-12-12 19:37:09 +0200

    End of lession 21.

--- tool_executor.py
+++ tool_executor.py
@@ -1,8 +1,9 @@
+import json
 from collections import defaultdict
 from typing import List
 
 from dotenv import load_dotenv
-from langchain_community.tools import TavilySearchResults
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
 from langchain_core.messages import BaseMessage, ToolMessage, HumanMessage, AIMessage
 from langgraph.prebuilt import ToolInvocation, ToolExecutor
@@ -17,20 +18,18 @@ tavily_tool = TavilySearchResults(api_wrapper=search, max_results=5)
 tool_executor = ToolExecutor([tavily_tool])
 
 
-
 def execute_tools(state: List[BaseMessage]) -> List[ToolMessage]:
     tool_invocation: AIMessage = state[-1]
-    parsed_tool_calls = parser.invoke(tool_invocation)
 
+    parsed_tool_calls = parser.invoke(tool_invocation)
     ids = []
     tool_invocations = []
-
     for parsed_call in parsed_tool_calls:
         for query in parsed_call["args"]["search_queries"]:
             tool_invocations.append(
                 ToolInvocation(
                     tool="tavily_search_results_json",
-                    tool_input = query,
+                    tool_input=query,
                 )
             )
             ids.append(parsed_call["id"])
@@ -41,14 +40,22 @@ def execute_tools(state: List[BaseMessage]) -> List[ToolMessage]:
     outputs_map = defaultdict(dict)
     for id_, output, invocation in zip(ids, outputs, tool_invocations):
         outputs_map[id_][invocation.tool_input] = output
-    pass
+
+    # Convert the mapped outputs to ToolMessage objects
+    tool_messages = []
+    for id_, mapped_output in outputs_map.items():
+        tool_messages.append(
+            ToolMessage(content=json.dumps(mapped_output), tool_call_id=id_)
+        )
+
+    return tool_messages
 
 
 if __name__ == "__main__":
     print("Tool Executor Enter")
 
     human_message = HumanMessage(
-        content="Write about AI-Powered SOC / autonomous soc problem domain,"
+        content="Write about AI-Powered SOC / autonomous soc  problem domain,"
         " list startups that do that and raised capital."
     )
 
@@ -58,9 +65,9 @@ if __name__ == "__main__":
         search_queries=[
             "AI-powered SOC startups funding",
             "AI SOC problem domain specifics",
-            "Technologies used by AI-powered SOC startups"
+            "Technologies used by AI-powered SOC startups",
         ],
-        id="Fred",
+        id="call_KpYHichFFEmLitHFvFhKy1Ra",
     )
 
     raw_res = execute_tools(
@@ -71,11 +78,11 @@ if __name__ == "__main__":
                 tool_calls=[
                     {
                         "name": AnswerQuestion.__name__,
-                        "args": answer.model_dump(),
-                        "id": "Fred",
+                        "args": answer.dict(),
+                        "id": "call_KpYHichFFEmLitHFvFhKy1Ra",
                     }
-                ]
-            )
+                ],
+            ),
         ]
     )
     print(raw_res)
